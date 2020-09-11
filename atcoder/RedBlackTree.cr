81df563fa19cf6e4f62a80a66c7dd114a6671d7f
Koki Takahashi <hakatasiloving@gmail.com>
2020-09-11 09:12:08 +0900

    Add license headers

--- atcoder/RedBlackTree.cr
+++ atcoder/RedBlackTree.cr
@@ -1,12 +1,25 @@
+# ac-library.cr by hakatashi https://github.com/hakatashi/ac-library.cr
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # bench_red_black_tree.rb by Rubinius Project, (c) Rubinius Contributors
 # https://github.com/rubinius/rubinius-benchmark/blob/master/real_world/bench_red_black_tree.rb
 # Licensed under BSD License https://spdx.org/licenses/BSD-3-Clause.html
 
 # red_black_tree.cr by Crystal Language Project, (c) Manas Technology Solutions
 # https://github.com/crystal-lang/crystal/blob/master/samples/red_black_tree.cr
-# Licensed under MIT License https://mit-license.org
-
-# RedBlackTree.cr by Koki Takahashi
 # Licensed under MIT License https://mit-license.org
 
 class RedBlackTree
