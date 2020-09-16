617acba1bee10561fee05236a0b695eb009291d1
Koki Takahashi <hakatasiloving@gmail.com>
2020-09-16 10:03:58 +0900

    Setup oj-verify

---
+++ .verify-helper/build.sh
@@ -0,0 +1,15 @@
+filename="$1"; shift
+output="$1"; shift
+
+if [ "$filename" -nt "${output}" ]; then
+  echo "Building Crystal project ${filename}..."
+  if [ -d /usr/local/share/crystal-0.33.0-1 ]; then
+    /usr/local/share/crystal-0.33.0-1/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
+  elif [ -d /tmp/crystal-0.33.0-1 ]; then
+    /tmp/crystal-0.33.0-1/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
+  else
+    docker run -v "$PWD":/mnt crystallang/crystal:0.33.0 crystal build "/mnt/$filename" -o "/mnt/${output}" --error-trace $@ || exit 1
+  fi
+fi
+
+echo "Build completed."
