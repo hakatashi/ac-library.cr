39837f77b408f7e531c75db3465e958e7e3c101c
Koki Takahashi <hakatasiloving@gmail.com>
2021-03-04 22:10:52 +0900

    Improve Rho algorithm by randomizing seed of f function

--- atcoder/Prime.cr
+++ atcoder/Prime.cr
@@ -72,23 +72,33 @@ module AtCoder
     end
 
     private def find_factor(n : Int)
-      # Factor of 4 cannot be discovered by Pollard's Rho with f(x) = x^x+1
-      if n == 4
+      # Factor of 2*n cannot be discovered by Pollard's Rho with f(x) = x^x+n-1
+      if n.even?
         typeof(n).new(2)
       else
-        pollard_rho(n).not_nil!
+        factor = pollard_rho(n)
+        if factor.nil?
+          puts "Factor not found: #{n}"
+        end
+        factor.not_nil!
       end
+    end
+
+    private def get_max(n : T) forall T
+      T::MAX
     end
 
     # Get single factor by Pollard's Rho Algorithm
     private def pollard_rho(n : Int)
+      seed = (rand(n - 1) + 1) % (get_max(n) - n)
+
       typeof(n).new(1).upto(n) do |i|
         x = i
-        y = pollard_random_f(x, n)
+        y = pollard_random_f(x, n, seed)
 
         loop do
-          x = pollard_random_f(x, n)
-          y = pollard_random_f(pollard_random_f(y, n), n)
+          x = pollard_random_f(x, n, seed)
+          y = pollard_random_f(pollard_random_f(y, n, seed), n, seed)
           gcd = (x - y).gcd(n)
 
           if gcd == n
@@ -102,8 +112,9 @@ module AtCoder
       end
     end
 
-    private def pollard_random_f(n : Int, mod : Int)
-      (mul_mod(n, n, mod) + 1) % mod
+    @[AlwaysInline]
+    private def pollard_random_f(n : Int, mod : Int, seed : Int)
+      (mul_mod(n, n, mod) + seed) % mod
     end
 
     private def extract_prime_division_base(prime_divisions_class : Array({T, T}).class) forall T
