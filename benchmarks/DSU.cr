47a59bc134cebbf2f8c36a436479904fb50bcc26
Koki Takahashi <hakatasiloving@gmail.com>
2021-06-12 10:30:26 +0900

    Remove unneeded asterisk import in benchmarks/DSU.cr

--- benchmarks/DSU.cr
+++ benchmarks/DSU.cr
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require "../atcoder/*"
+require "../atcoder/DSU.cr"
 require "benchmark"
 require "spec"
 
