d749b832c2bd9cec064644e70d8be3907af39725
Koki Takahashi <hakatasiloving@gmail.com>
2023-01-21 13:20:28 +0000

    Add verifier of Convolution

---
+++ verify/convolution_mod.test.cr
@@ -0,0 +1,26 @@
+# ac-library.cr by hakatashi https://github.com/google/ac-library.cr
+#
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/convolution_mod
+
+require "../src/Convolution.cr"
+require "../src/ModInt.cr"
+
+n, m = read_line.split.map(&.to_i64)
+ais = read_line.split.map {|n| AtCoder::ModInt998244353.new(n.to_i64)}
+bs = read_line.split.map {|n| AtCoder::ModInt998244353.new(n.to_i64)}
+cs = AtCoder::Convolution.convolution(ais, bs)
+puts cs.map(&.to_s).join(" ")
