182b49268f3afdd7525f888d63c89f1530de6350
ngng628 <ngng628.kyopro@gmail.com>
2023-06-04 01:42:07 +0900

    Fix typo

--- .verify-helper/build.sh
+++ .verify-helper/build.sh
@@ -13,7 +13,7 @@ if [ "$filename" -nt "${output}" ]; then
     /usr/local/share/crystal-1.7.3/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
   elif [ -d /tmp/crystal-1.7.3 ]; then
     /tmp/crystal-1.7.3/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
-  elif [[ "`crystal version`" == *"1.7.2"* ]]; then
+  elif [[ "`crystal version`" == *"1.7.3"* ]]; then
     crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
   else
     docker run -v "$PWD":/mnt crystallang/crystal:0.33.0 crystal build "/mnt/$filename" -o "/mnt/${output}" --error-trace $@ || exit 1
