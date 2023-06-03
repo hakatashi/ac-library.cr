6d7698f11625f8655c758d9effdfed27f0f4a450
ngng628 <ngng628.kyopro@gmail.com>
2023-06-04 01:24:43 +0900

    Fix crystal version

--- .verify-helper/build.sh
+++ .verify-helper/build.sh
@@ -9,10 +9,10 @@ if [ "$filename" -nt "${output}" ]; then
     /tmp/crystal-0.33.0-1/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
   elif [[ "`crystal version`" == *"0.33.0"* ]]; then
     crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
-  elif [ -d /usr/local/share/crystal-1.7.2-1 ]; then
-    /usr/local/share/crystal-1.7.2-1/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
-  elif [ -d /tmp/crystal-1.7.2-1 ]; then
-    /tmp/crystal-1.7.2-1/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
+  elif [ -d /usr/local/share/crystal-1.7.3 ]; then
+    /usr/local/share/crystal-1.7.3/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
+  elif [ -d /tmp/crystal-1.7.3 ]; then
+    /tmp/crystal-1.7.3/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
   elif [[ "`crystal version`" == *"1.7.2"* ]]; then
     crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
   else
