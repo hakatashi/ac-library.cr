7e6818329985535a123925d040ddd4bee2d690af
ngng628 <ngng628.kyopro@gmail.com>
2023-06-20 11:27:55 +0900

    Revert crystal version

--- .verify-helper/build.sh
+++ .verify-helper/build.sh
@@ -9,11 +9,11 @@ if [ "$filename" -nt "${output}" ]; then
     /tmp/crystal-0.33.0-1/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
   elif [[ "`crystal version`" == *"0.33.0"* ]]; then
     crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
-  elif [ -d /usr/local/share/crystal-1.7.3 ]; then
-    /usr/local/share/crystal-1.7.3/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
-  elif [ -d /tmp/crystal-1.7.3 ]; then
-    /tmp/crystal-1.7.3/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
-  elif [[ "`crystal version`" == *"1.7.3"* ]]; then
+  elif [ -d /usr/local/share/crystal-1.7.2-1 ]; then
+    /usr/local/share/crystal-1.7.2-1/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
+  elif [ -d /tmp/crystal-1.7.2-1 ]; then
+    /tmp/crystal-1.7.2-1/bin/crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
+  elif [[ "`crystal version`" == *"1.7.2"* ]]; then
     crystal build "$filename" -o "${output}" --error-trace $@ || exit 1
   else
     docker run -v "$PWD":/mnt crystallang/crystal:0.33.0 crystal build "/mnt/$filename" -o "/mnt/${output}" --error-trace $@ || exit 1
