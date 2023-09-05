95d143cbd1fd7a52390d9bc87a33fd1711432be8
Koki Takahashi <hakatasiloving@gmail.com>
2023-09-05 03:07:33 +0000

    Add Convolution benchmark script

---
+++ benchmarks/Convolution.cr
@@ -0,0 +1,31 @@
+# ac-library.cr by hakatashi https://github.com/google/ac-library.cr
+#
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "../src/mod_int.cr"
+require "../src/convolution.cr"
+require "benchmark"
+require "spec"
+
+Benchmark.bm do |x|
+  n = 1_000_000_i64
+  ais = Array(AtCoder::ModInt998244353).new(n) { |i| AtCoder::ModInt998244353.new(i.to_i64) }
+  bs = Array(AtCoder::ModInt998244353).new(n) { |i| AtCoder::ModInt998244353.new(i.to_i64) }
+
+  # O(nlogn+logMOD)
+  x.report("AtCoder::Convolution") do
+    AtCoder::Convolution.convolution(ais, bs)
+  end
+end
